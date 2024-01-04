09a74c9d7f5d155b89f9d744601cc951eaee4c31
TejasBhovad <tejasbhovad@gmail.com>
2024-01-04 19:06:35 +0530

    style: mobile sidebar

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -7,26 +7,52 @@ import "../styles/utils.css";
 
 const Sidebar = () => {
   return (
-    <div className="hidden md:flex absolute h-full w-full top-[40%] mx-4 w-12 h-44   rounded-md bg-opacity-25 flex-col items-center justify-center gap-2 px-0.5">
-      <Link
-        href="/"
-        className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent hover:bg-opacity-25 circle-transform"
-      >
-        <Home />
-      </Link>
-      <Link
-        href="/scanner"
-        className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent hover:bg-opacity-25 circle-transform"
-      >
-        <Scanner />
-      </Link>
-      <Link
-        href="/generator"
-        className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent transition-all hover:bg-opacity-25 circle-transform"
-      >
-        <Upload />
-      </Link>
-    </div>
+    <section>
+      <div className="hidden md:flex absolute h-full w-full top-[40%] mx-4 w-12 h-44   rounded-md bg-opacity-25 flex-col items-center justify-center gap-2 px-0.5">
+        <Link
+          href="/"
+          className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent hover:bg-opacity-25 circle-transform"
+        >
+          <Home />
+        </Link>
+        <Link
+          href="/scanner"
+          className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent hover:bg-opacity-25 circle-transform"
+        >
+          <Scanner />
+        </Link>
+        <Link
+          href="/generator"
+          className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent transition-all hover:bg-opacity-25 circle-transform"
+        >
+          <Upload />
+        </Link>
+      </div>
+      {/* for mobile devices sidebar should be at bottom center */}
+      <div className="w-full items-center flex justify-center">
+        {" "}
+        <div className="flex md:hidden absolute  bottom-8 mx-4 h-12 w-48 rounded-md bg-opacity-25 items-center justify-center gap-4 px-0.5">
+          <Link
+            href="/"
+            className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent hover:bg-opacity-25 circle-transform"
+          >
+            <Home />
+          </Link>
+          <Link
+            href="/scanner"
+            className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent hover:bg-opacity-25 circle-transform"
+          >
+            <Scanner />
+          </Link>
+          <Link
+            href="/generator"
+            className="w-full bg-secondary bg-opacity-30 aspect-square rounded-lg flex items-center justify-center hover:bg-accent transition-all hover:bg-opacity-25 circle-transform"
+          >
+            <Upload />
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
