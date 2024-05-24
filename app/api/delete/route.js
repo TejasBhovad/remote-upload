19612b9ec37d9ece3fb7518e8b30d4ba78926d56
TejasBhovad <tejasbhovad@gmail.com>
2024-05-24 21:08:33 +0530

    fix: put back message

--- app/api/delete/route.js
+++ app/api/delete/route.js
@@ -4,7 +4,7 @@ export async function GET(request) {
   const authHeader = request.headers.get("authorization");
 
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response(authHeader, {
+    return new Response("Unauthorized", {
       status: 401,
     });
   }
