0954c4a6ec5ca164f13eef0000f9ffd4146034fe
TejasBhovad <tejasbhovad@gmail.com>
2025-10-02 00:29:21 +0530

    chore: init

--- src/utils/uploadthing.js
+++
@@ -1,9 +0,0 @@
-import {
-  generateUploadButton,
-  generateUploadDropzone,
-} from "@uploadthing/react";
-import { generateReactHelpers } from "@uploadthing/react";
-
-export const UploadButton = generateUploadButton();
-export const UploadDropzone = generateUploadDropzone();
-export const { useUploadThing } = generateReactHelpers();
