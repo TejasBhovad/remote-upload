df7d4fcb57a3b1a39240091fdf5a9ede636e1d4d
TejasBhovad <tejasbhovad@gmail.com>
2025-10-13 00:57:53 +0530

    chore: update tests

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -8,7 +8,9 @@ test("should navigate to the upload page", async ({ page }) => {
   await page.getByRole("link", { name: "Share files" }).click();
 
   await expect(page).toHaveURL("https://localhost:3000/upload");
-  await expect(page.locator("h1")).toContainText("upload files here");
+  await expect(page.locator("h3")).toContainText(
+    "Drag & Drop files here or click to browse"
+  );
 });
 
 test("should navigate to the scan page", async ({ page }) => {
@@ -19,5 +21,5 @@ test("should navigate to the scan page", async ({ page }) => {
   await page.getByRole("link", { name: "Receive files" }).click();
 
   await expect(page).toHaveURL("https://localhost:3000/scan");
-  await expect(page.locator("h1")).toContainText("scan code here");
+  await expect(page.locator("h1")).toContainText("Enter Your Code");
 });
